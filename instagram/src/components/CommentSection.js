64974047bf41929e230eb61671ae41377f0afd81
unknown <eriznis@gmail.com>
2019-02-18 18:46:18 -0500

    search bar needed

--- instagram/src/components/CommentSection.js
+++ instagram/src/components/CommentSection.js
@@ -5,11 +5,14 @@ const Comment = props => {
     return (
         <div>
             {props.comments.map(individualCommenter => (
-                <div>
+                <div className='comment'>
                     {individualCommenter.username}
                     {individualCommenter.text}
                 </div>)
             )}
+            <form>
+                <input placeholder='Add a comment...'></input>
+            </form>
         </div>
         
     )
