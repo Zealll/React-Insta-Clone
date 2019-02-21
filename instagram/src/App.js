1dd9c1f6f45e22c6ed59da4d4540dfa505f8ce4a
unknown <eriznis@gmail.com>
2019-02-20 20:47:39 -0500

    Posts page created, and App.js refractored

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react';
-import dummyData from './dummy-data';
+import PostsPage from './components/PostContainer/PostsPage'
 import './App.css';
-import PostContainer from './components/PostContainer/PostContainer'
-import SearchBar from './components/SearchBar/SearchBar'
-
 class App extends Component {
   constructor() {
     super();
-    this.state ={
-      data: [],
-      filteredUsers: [],
-      searchInput: '',
-    }
+    this.state = {}
   }
 
-  componentDidMount() {
-    this.setState({data: dummyData})
-  }
 
-  changeHandler = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  searchHandler = e => {
-    this.changeHandler(e)
-    this.setState((prevState) => {
-      const filteredUsersArray =  prevState.data.filter(post => post.username.includes(prevState.searchInput))
-      return {filteredUsers: filteredUsersArray}
-    })
-  }
-
+  
   
    render() {
      console.log(this.state.data)
     return (
       <div className="App">
-        <SearchBar searchHandler={this.searchHandler} searchInput={this.state.searchInput}/>
-        <PostContainer data={this.state.filteredUsers.length > 0 ? this.state.filteredUsers : this.state.data}/>
+      <PostsPage />
+        
       </div>
       
     );
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import dummyData from '../../dummy-data';
+// import './App.css';
+import PostContainer from './PostContainer'
+import SearchBar from '../SearchBar/SearchBar'
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state ={
+      data: [],
+      filteredUsers: [],
+      searchInput: '',
+    }
+  }
+
+  componentDidMount() {
+    this.setState({data: dummyData})
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  searchHandler = e => {
+    this.changeHandler(e)
+    this.setState((prevState) => {
+      const filteredUsersArray =  prevState.data.filter(post => post.username.includes(prevState.searchInput))
+      return {filteredUsers: filteredUsersArray}
+    })
+  }
+
+  
+   render() {
+     console.log(this.state.data)
+    return (
+      <div className="App">
+        <SearchBar searchHandler={this.searchHandler} searchInput={this.state.searchInput}/>
+        <PostContainer data={this.state.filteredUsers.length > 0 ? this.state.filteredUsers : this.state.data}/>
+      </div>
+      
+    );
+  }
+}
+
+export default PostsPage;
